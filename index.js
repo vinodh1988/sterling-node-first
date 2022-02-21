5bfa38ac5cd36f0549cd0311e347167c0e05c62c
vinodh1988 <k.c.vinodh@hotmail.com>
2022-02-21 15:57:45 +0530

    url-pattern

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const { response } = require("express")
 const express=require("express")
 const app  = express()
 
+app.get("/greet",function(request,response){
+    response.send("How are you!!! Welcome!!!")
+})
+app.get("/",function(request,response){
+     response.send("Hello!!! Node js is running...")
+})
 app.listen("8000",function(){
     console.log("server started in port number 8000")
 })
