3f1bbb226d5fc32996f272d4446ae0eea075a215
vinodh1988 <k.c.vinodh@hotmail.com>
2022-02-21 16:41:20 +0530

    first-commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
-
 const express=require("express")
 const app  = express()
 const path =require("path")
 
+app.use(express.static(path.join(__dirname,"public/styles")))
+app.use(express.static(path.join(__dirname,"public/scripts")))
 app.get("/home",function(request,response){
     response.sendFile(path.join(__dirname,"public/index.html"))
 })
@@ -13,6 +13,7 @@ app.get("/greet",function(request,response){
 app.get("/",function(request,response){
      response.send("Hello!!! Node js is running...")
 })
+
 app.listen("8000",function(){
     console.log("server started in port number 8000")
 })
